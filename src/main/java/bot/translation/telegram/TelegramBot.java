475af9f72ece7d4c7929eae19af2b7fbbd9271be
Santrosyan03 <sevaksantrosyan5@gmail.com>
2024-02-09 16:17:17 +0400

    Secured Token

--- src/main/java/bot/translation/telegram/TelegramBot.java
+++ src/main/java/bot/translation/telegram/TelegramBot.java
@@ -1,16 +1,21 @@
 package bot.translation.telegram;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 
 public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         String chatId = update.getMessage().getChatId().toString();
         String text = update.getMessage().getText();
-
+        text = translate(text);
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
         sendMessage.setText(text);
@@ -23,12 +28,27 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     @Override
-    public String getBotToken() {
-        return "6690156151:AAERI57Y6UagMbwKOdTdalWFjLLjBbdgJD0";
+    public String getBotUsername() {
+        return "TranslationTelegramBot";
     }
 
     @Override
-    public String getBotUsername() {
-        return "TranslationTelegramBot";
+    public String getBotToken() {
+        Properties prop = new Properties();
+        try {
+            prop.load(TelegramBot.class.getClassLoader().getResourceAsStream("personal.properties"));
+            System.out.println(prop.getProperty("token"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return prop.getProperty("token");
+    }
+
+    public String translate(String text) {
+        Locale locale = new Locale.Builder().setLanguage("fr").setRegion("CA").build();
+        ResourceBundle bundle = ResourceBundle.getBundle("translations", locale);
+        String translatedText = bundle.getString("hello");
+        System.out.println("Translated text: " + translatedText);
+        return translatedText;
     }
 }
